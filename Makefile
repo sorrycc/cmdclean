a7a5cb0b77c1573b3cad1240740dcf1ac7856c58
chencheng <sorrycc@gmail.com>
2014-08-19 14:39:08 +0800

    add testcase

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -R spec -t 20000
+	./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -R spec -t 20000 --require should --inline-diffs
 
 coveralls: test
 	cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
--- test/index.js
+++ test/index.js
@@ -1,12 +1,22 @@
 'use strict';
 
-var should = require('should');
 var cmdclean = require('..');
+var join = require('path').join;
+var fs = require('fs');
 
 describe('cmdclean', function() {
 
   it('Normal use', function() {
-
+    var result = cmdclean({
+      filePath: join(__dirname, 'fixtures/normal.js'),
+      createAnonymousAMDModule: true
+    });
+    assets(result, 'normal.js');
   });
+
+  function assets(actual, dest) {
+    var expected = fs.readFileSync(join(__dirname, 'fixtures/expect/', dest), 'utf-8');
+    actual.should.be.equal(expected);
+  }
 
 });
