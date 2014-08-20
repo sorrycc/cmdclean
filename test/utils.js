93eb443218b4c157596d47fdadd95f6195022bcf
chencheng <sorrycc@gmail.com>
2014-08-20 14:50:58 +0800

    add utils.getRequires

---
+++ test/utils.js
@@ -0,0 +1,24 @@
+var esprima = require('esprima');
+var utils = require('../lib/utils');
+
+describe.only('utils', function() {
+
+  it('getRequires', function() {
+    var ast;
+
+    ast = esprima.parse('require("a");');
+    utils.getRequires(ast).should.be.eql(['a']);
+
+    ast = esprima.parse('require("a");require("b");');
+    utils.getRequires(ast).should.be.eql(['a','b']);
+
+    ast = esprima.parse('if(false)require("a");');
+    utils.getRequires(ast).should.be.eql(['a']);
+
+    ast = esprima.parse('/*require("a");*/');
+    utils.getRequires(ast).should.be.eql([]);
+
+    ast = esprima.parse('/require("a");/');
+    utils.getRequires(ast).should.be.eql([]);
+  });
+});
