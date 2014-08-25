ee527098ed28ec6fa59e88ab62ad59d03a0f5093
chencheng <sorrycc@gmail.com>
2014-08-25 16:57:44 +0800

    support return value with exports assignment

--- test/fixtures/expect/nodeps.js
+++ test/fixtures/expect/nodeps.js
@@ -1,13 +1,13 @@
 ;(function() {
 var a_100_indexjs, _a_100_relativejs_;
 _a_100_relativejs_ = function (exports) {
-  exports = 'relative';
+  exports.val = 'relative';
   return exports;
-}();
+}({});
 a_100_indexjs = function (exports) {
   'use strict';
   var a_100_relativejs = _a_100_relativejs_;
-  exports = $('foo' + a_100_relativejs);
+  exports = $('foo' + a_100_relativejs.val);
   return exports;
 }();
 }());
--- test/fixtures/nodeps.js
+++ test/fixtures/nodeps.js
@@ -1,8 +1,8 @@
 define('a/1.0.0/index.js', [], function(require, module, exports) {
   'use strict';
   var a_100_relativejs = require('a/1.0.0/relative.js');
-  module.exports = $('foo' + a_100_relativejs);
+  module.exports = $('foo' + a_100_relativejs.val);
 });
 define('a/1.0.0/relative.js', [], function(require, module, exports) {
-  module.exports = 'relative';
+  exports.val = 'relative';
 });
